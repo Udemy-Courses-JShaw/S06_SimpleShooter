44608c6be9ee7b7e872c9cc01b79d7c3b3dbd9c0
Jarrod G Shaw <frail42@gmail.com>
2020-07-30 11:21:59 -0700

    New BTTask BUG - Crashes

--- Source/S06_SimpleShooter/S06_SimpleShooter.Build.cs
+++ Source/S06_SimpleShooter/S06_SimpleShooter.Build.cs
@@ -8,7 +8,7 @@ public class S06_SimpleShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
